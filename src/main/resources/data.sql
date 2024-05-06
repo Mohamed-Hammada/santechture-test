d11774f6fb8beac46f4b758c58448cfe6a9d0ed2
Mohamed-Hammada <mohammed.hammada.2012@gmail.com>
2024-05-06 20:14:39 +0300

    update admin password encrypted

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,4 @@ INSERT INTO user (user_id,username, email) VALUES (1000001,'Mohammed', 'mohammed
 INSERT INTO user (user_id,username, email) VALUES (1000002,'Ali', 'ali@santecture.com');
 INSERT INTO user (user_id,username, email) VALUES (1000003,'Adam', 'adam@santecture.com');
 
-INSERT INTO admin (username, password) VALUES ('Admin', 'p@ssw0rd');
+INSERT INTO admin (username, password) VALUES ('Admin', '$2a$10$KNpmK64SNRv9Hc2E8fIFVOvRLV43nNPgF8DgUFNoxgA/lsy/.9wIi');
